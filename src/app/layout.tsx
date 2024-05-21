f578614b5adb3cb07c5d70245ba17643890e1557
Luca Aflaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2024-05-20 21:05:05 -0300

    Ajuste de rotas e proividers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Toaster } from "sonner";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'sonner';
+import './globals.css';
+import Providers from './providers';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "WikiPet",
-  description: "WikiPet",
+  title: 'WikiPet',
+  description: 'WikiPet',
 };
 
 export default function RootLayout({
@@ -16,10 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang='pt-BR'>
       <body className={inter.className}>
         <Toaster richColors closeButton />
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-export default function SignInLayout({
+export default function Providers({
   children,
 }: Readonly<{
   children: React.ReactNode;
