1fe8538a999aea8f7267c3e047da04b26c042a82
pietr <pietrokucharski@hotmail.com>
2024-05-25 20:38:16 -0300

    feat: ajuste na tela de loading

--- src/app/pets/loading.tsx
+++ src/app/pets/loading.tsx
@@ -1,16 +1,14 @@
-// loading.tsx
+// components/Loading.tsx
 
-import React from 'react';
-import Image from 'next/image';
+import React from "react"
+import { Loader } from "lucide-react"
 
 const Loading: React.FC = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
-      <div className="border-4 border-white border-opacity-30 rounded-full w-16 h-16 flex justify-center items-center">
-        <p>loading</p>
-      </div>
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-transparent z-50">
+      <Loader className="animate-spin text-black" />
     </div>
-  );
-};
+  )
+}
 
-export default Loading;
+export default Loading
